f98ec5c7361e9428882aace94e6e83aa6eac0894
Anwar Sadat Ayub <anwar.rhsl@gmail.com>
2024-03-20 13:28:53 +0000

    Adding query parameter to prevent sql injection

--- index.php
+++ index.php
@@ -7,8 +7,13 @@ $config = require('config.php');
 
 $db = new Database($config['database']);
 
-$posts = $db->query("select * from post")->fetchAll();
+$id =$_GET['id'];
+$query = "select * from post where id  = ?";
 
+$posts = $db->query($query, [$id])->fetch();
+
+dd($posts);
+ 
 foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
+    echo "<li><b>" . $post['title'] . "</b></li>";
 }
