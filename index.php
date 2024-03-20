e47df65c90a69f194db15f8736a7196d5f3a4c5c
Anwar Sadat Ayub <anwar.rhsl@gmail.com>
2024-03-20 13:32:10 +0000

    Updated the query parameter to :id and passing and associative array

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $id =$_GET['id'];
-$query = "select * from post where id  = ?";
+$query = "select * from post where id  = :id";
 
-$posts = $db->query($query, [$id])->fetch();
+$posts = $db->query($query, ['id' => $id])->fetch();
 
 dd($posts);
  
