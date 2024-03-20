0552551ea90e6d181aa4ee1ac714a5b298545be9
Anwar Sadat Ayub <anwar.rhsl@gmail.com>
2024-03-20 21:09:39 +0000

    Added authorization when viewing notes

--- views/note.view.php
+++ views/note.view.php
@@ -4,8 +4,8 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>
-            <a href="/post" class="hover:bg-blue-400 rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm my-6">
+        <p class="mb-6">
+            <a href="/post" class="hover:bg-blue-400 rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
                
                 Back
             </a>
