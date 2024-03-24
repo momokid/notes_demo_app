1691bc728364963a2ee7b62bbd39ae02e75459d8
Anwar Sadat Ayub <anwar.rhsl@gmail.com>
2024-03-24 15:01:14 +0000

    Update Notes route

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -5,19 +5,25 @@ use Core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
-$currentUserId = 1 ;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //form was submitted. Delete the current request
+    $db->query("DELETE FROM notes WHERE id= :id", $_GET);
+}else{
+
+$currentUserId = 1;
 
 $note = $db->query('SELECT * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
 
-if(!$note){
+if (!$note) {
     abort();
 }
-
+}
 authorize($note['user_id'] == $currentUserId);
 
 // if($note['user_id'] != $currentUserId){
 //     abort(Response::FORBIDDEN);
 // }
 
-view('notes/show.view.php', ['heading' => 'Note', 'note' => $note]);
 
+view('notes/show.view.php', ['heading' => 'Note', 'note' => $note]);
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -6,13 +6,15 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <p class="mb-6">
-            <a href="/note" class="hover:bg-blue-400 rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-               
+            <a href="/notes" class="hover:bg-blue-400 rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
                 Back to notes
             </a>
         </p>
         <p><?= htmlspecialchars($note['body']); ?></p>
-    <form action="" class="hover:bg-red-400 rounded-md bg-red-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">Delete</form>
+    <form >
+        <input type="text" value="<?= $note['id'] ?>" />
+        <button method="POST" class="hover:bg-red-400 rounded-md bg-red-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">Delete</button>
+    </form>
     </div>
 </main>
 
